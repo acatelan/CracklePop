57b03aad8d0287b31a5f81f65835fc2e27ae334b
acatelan <38012842+acatelan@users.noreply.github.com>
2023-10-16 19:19:22 -0700

    Update CracklePop.cpp
    uploaded the wrong file as cpp, fixed

--- CracklePop.cpp
+++ CracklePop.cpp
@@ -1,1 +1,20 @@
-#include "CracklePop.h"
+#include <iostream>
+
+int main() {
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0) {
+			if (i % 5 == 0) {
+				std::cout << "CracklePop" << std::endl;
+			}
+			else {
+				std::cout << "Crackle" << std::endl;
+			}
+		}
+		else if (i % 5 == 0) {
+			std::cout << "Pop" << std::endl;
+		}
+		else {
+			std::cout << i << std::endl;
+		}
+	}
+}
